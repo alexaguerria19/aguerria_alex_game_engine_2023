03c3d1b07dd31406ef36a07c8c908334b91082ec
alexaguerria19 <142263771+alexaguerria19@users.noreply.github.com>
2023-11-16 14:41:19 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,14 @@
 # game settings 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 400
+HEIGHT = 650
 FPS = 30
-SCORE = 0
 
 # player settings
-PLAYER_JUMP = 20
-PLAYER_GRAV = 1.5
+PLAYER_JUMP = 25
+PLAYER_GRAV = 1
+PLAYER_FRIC = 0.2
+PLAYER_SIZE = 50
+JUMP_STRENGTH = -15
 
 # define colors
 WHITE = (255, 255, 255)
@@ -14,10 +16,12 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-ORANGE = (255, 165, 0)
 
-PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, "normal"),
-                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, "normal"),
+
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, "moving"),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20,"moving"),
                  (125, HEIGHT - 350, 100, 20, "normal"),
-                 (350, 200, 100, 20, "moving"),
-                 (175, 100, 50, 20, "normal")]
+                 (222, 200, 100, 20, "moving"),
+                 (175, 100, 50, 20, "moving")]
+
+                
