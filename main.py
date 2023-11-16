f35be371db3ddcdea299249d38a199f3a01f349c
alexaguerria19 <142263771+alexaguerria19@users.noreply.github.com>
2023-11-16 14:41:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+# test
 #This file was created by: Alex Aguerria
+
+#GameDesign: 
+#Goals get as far up as you can 
+
+    #   Jump and dont fall
+    #   Move upwards with random platforms
+    #   Score for every new platform jumped on
+    
+
 
 # import libraries and modules
 import pygame as pg
 from pygame.sprite import Sprite
+import random
 from random import randint
 import os
 from settings import *
+from sprites import *
+import math
+
 
 vec = pg.math.Vector2
 
@@ -15,186 +29,170 @@ img_folder = os.path.join(game_folder, 'images')
 snd_folder = os.path.join(game_folder, 'sounds')
 
 
-def draw_text(text, size, color, x, y):
-    font_name = pg.font.match_font('arial')
-    font = pg.font.Font(font_name, size)
-    text_surface = font.render(text, True, color)
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (x,y)
-    screen.blit(text_surface, text_rect)
-
-class Player(Sprite):
-    def __init__(self):
-        Sprite.__init__(self)
-        # self.image = pg.Surface((50, 50))
-        # self.image.fill(GREEN)
-        # use an image for player sprite...
-        self.image = pg.image.load(os.path.join(img_folder, 'theBell.png')).convert()
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-        self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
-        print(self.rect.center)
-        self.hitpoints = 100
-    def controls(self):
-        keys = pg.key.get_pressed()
-        if keys[pg.K_a]:
-            self.acc.x = -5
-        if keys[pg.K_d]:
-            self.acc.x = 5
-        if keys[pg.K_SPACE]:
-            self.jump()
-    def jump(self):
-        hits = pg.sprite.spritecollide(self, all_platforms, False)
-        if hits:
-            print("i can jump")
-            self.vel.y = -PLAYER_JUMP
-    def update(self):
-        self.acc = vec(0,PLAYER_GRAV)
-        self.controls()
-        # if friction - apply here
-        self.acc.x += self.vel.x * -0.2
-        # this would only apply when doing a top down video game
-        # self.acc.y += self.vel.y * -0.2
-        # equations of motion
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
-        self.rect.midbottom = self.pos
-
-# platforms
-
-class Platform(Sprite):
-    def __init__(self, x, y, w, h, category):
-        Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
-        self.image.fill(WHITE)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        print(self.rect.center)
-        self.category = category
-        self.speed = 0
-        if self.category == "moving":
-            self.speed = 5
-    def update(self):
-        if self.category == "moving":
-            self.rect.x += self.speed
-            if self.rect.x > WIDTH or self.rect.x < 0:
-                self.speed = -self.speed
-        if self.category == "lava":
-            self.image.fill(ORANGE)
-
-class Mob(Sprite):
-    def __init__(self, x, y, w, h, category):
-        Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
-        self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        print(self.rect.center)
-        self.category = category
-        self.speed = 0
-        if self.category == "moving":
-            self.speed = 20
-    def update(self):
-        if self.category == "moving":
-            self.rect.x += self.speed
-            # check to see if this thing is on one side of the screen or the other
-            if self.rect.x > WIDTH or self.rect.x < 0:
-                self.speed = -self.speed
-                self.rect.y += self.rect.h
-            if self.rect.x > WIDTH/2:
-                self.image.fill(ORANGE)
-            else:
-                self.image.fill((randint(0,255), randint(0,255), randint(0,255)))
-
-
-
-# init pygame and create a window
+# Initialize Pygame
 pg.init()
-pg.mixer.init()
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("My Game...")
 
-clock = pg.time.Clock()
+# Set up the screen
+screen_width, screen_height = 800, 600
+screen = pg.display.set_mode((screen_width, screen_height))
+pg.display.set_caption("Pygame Start Button")
 
-# create a group for all sprites
-all_sprites = pg.sprite.Group()
-all_platforms = pg.sprite.Group()
-all_mobs = pg.sprite.Group()
-
-# instantiate classes
-player = Player()
-all_sprites.add(player)
-
-# add instances to groups
-
-# for i in range(0,10):
-#     p = Platform(randint(0,WIDTH), randint(0,HEIGHT), 100, 30, "moving")
-#     all_sprites.add(p)
-#     all_platforms.add(p)
-
-for i in range(0,10):
-    m = Mob(randint(0,WIDTH), randint(0,HEIGHT), 25, 25, "moving")
-    all_sprites.add(m)
-    all_mobs.add(m)
+# Button properties
+button_width, button_height = 200, 50
+button_x, button_y = (screen_width - button_width) // 2, (screen_height - button_height) // 2
+button_color = (0, 255, 0)  # Green color for the button
+button_text_color = (255, 255, 255)  # White text color
+button_font = pg.font.Font(None, 36)
+button_text = button_font.render("Start", True, button_text_color)
 
 
-for plat in PLATFORM_LIST:
-    p = Platform(*plat)
-    all_sprites.add(p)
-    all_platforms.add(p)
-
-
-# Game loop
-running = True
-while running:
-    # keep the loop running using clock
-    currentFPS = clock.tick(FPS)
-        
-    for event in pg.event.get():
-        # check for closed window
-        if event.type == pg.QUIT:
-            running = False
+class Game:
+    def __init__(self):
+        # init pygame and create a window
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("My Game...")
+        self.clock = pg.time.Clock()
+        self.running = True
     
-    ############ Update ##############
-    # update all sprites
-    all_sprites.update()
-    if player.rect.y > HEIGHT:
-        player.pos = vec(WIDTH/2, HEIGHT/2)
-    # this is what prevents the player from falling through the platform when falling down...
-    if player.vel.y > 0:
-            hits = pg.sprite.spritecollide(player, all_platforms, False)
+    def new(self):
+        # create a group for all sprites
+        self.bgimage = pg.image.load(os.path.join(img_folder, 'djbackground.png')).convert()
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+        self.all_platforms = pg.sprite.Group()
+        self.all_mobs = pg.sprite.Group()
+        self.all_invisible = pg.sprite.Group()
+        # instantiate classes
+        self.player = Player(self)
+        # add instances to groups
+        self.all_sprites.add(self.player)
+
+        for p in PLATFORM_LIST:
+            # instantiation of the Platform class
+            plat = Platform(*p)
+            self.all_sprites.add(plat)
+            self.all_platforms.add(plat)
+
+
+        for m in range(0,0):
+            m = Mob(randint(0, WIDTH), randint(0, math.floor(HEIGHT/2)), 20, 20, "normal")
+            self.all_sprites.add(m)
+            self.all_mobs.add(m)
+
+        for i in range(0,5):
+            i = invisible(randint(0, WIDTH), randint(0, math.floor(HEIGHT/2)), 20, 20, "movinginvisible")
+            self.all_sprites.add(i)
+            self.all_invisible.add(i)
+
+        self.run()
+    
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        self.all_sprites.update()
+        if self.player.pos.x < 0:
+            self.player.pos.x = WIDTH
+        if self.player.pos.x > WIDTH:
+            self.player.pos.x = 0
+        
+        # this is what prevents the player from falling through the platform when falling down...
+        if self.player.vel.y >= 0:
+            hits = pg.sprite.spritecollide(self.player, self.all_platforms, False)
             if hits:
-                player.pos.y = hits[0].rect.top
-                player.vel.y = 0
-                player.vel.x = hits[0].speed*1.5
+                self.player.pos.y = hits[0].rect.top
+                self.player.vel.y = 0
+                self.player.vel.x = hits[0].speed*1.5
+
+                    
+         # this prevents the player from jumping up through a platform
+        elif self.player.vel.y <= 0:
+            hits = pg.sprite.spritecollide(self.player, self.all_platforms, False)
+            if hits:
+                self.player.acc.y = -5
+                self.player.vel.y = 0
+                print("ouch")
+                self.score -1
+                if self.player.rect.bottom >= hits[0].rect.top - 1:
+                    self.player.rect.top = hits[0].rect.bottom
+ # move screen upward when player reaches the top
+        if self.player.rect.top <= HEIGHT / 4:
+            self.player.pos.y += abs(self.player.vel.y)
+            for plat in self.all_platforms:
+                plat.rect.y += abs(self.player.vel.y)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+                    self.score += 10
+# respawns the sprite to the start again
+        if self.player.rect.bottom > HEIGHT:
+            for sprite in self.all_sprites:
+                sprite.rect.y -= max(self.player.vel.y, 10)          
+                if sprite.rect.bottom < 0:
+                    sprite.kill()
+        if len(self.all_platforms) == 0:
+            self.playing = False
+
+# spawns in new platforms while moving up
+        while len(self.all_platforms) < 6:
+            width = random.randrange(50,100)
+            p = Platform(random.randrange(0, WIDTH - width),
+                         random.randrange(-75, -30),
+                         width, 20, "moving")
+            self.all_platforms.add(p)
+            self.all_sprites.add(p)
+
+# spawns in new platforms while moving up
+        while len(self.all_platforms) < 6:
+            width = random.randrange(50,100)
+            p = Platform(random.randrange(0, WIDTH - width),
+                         random.randrange(-75, -30),
+                         width, 20, "movinginvisible")
+            self.all_platforms.add(p)
+            self.all_sprites.add(p)
+
+
+    def events(self): 
+        for event in pg.event.get():
+        # check for closed window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
                 
-    # this prevents the player from jumping up through a platform
-    if player.vel.y < 0:
-        hits = pg.sprite.spritecollide(player, all_platforms, False)
-        if hits:
-            print("ouch")
-            SCORE -= 1
-            if player.rect.bottom >= hits[0].rect.top - 5:
-                player.rect.top = hits[0].rect.bottom
-                player.acc.y = 5
-                player.vel.y = 0
+    def draw(self):
+        ############ Draw ################
+        # draw the background screen
+        self.screen.fill(BLACK)
+        self.screen.blit(self.bgimage,(0,0))
+        # draw all sprites
+        self.all_sprites.draw(self.screen)
+        self.draw_text("Score: " + str(self.score), 22, BLACK, WIDTH/2, HEIGHT/7)
+        # buffer - after drawing everything, flip display
+        pg.display.flip()
+    
+    def draw_text(self, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface, text_rect)
 
-    mhits = pg.sprite.spritecollide(player, all_mobs, False)
-    if mhits:
-        print("ive collided with a mob")
-    ############ Draw ################
-    # draw the background screen
-    screen.fill(BLACK)
-    # draw all sprites
-    all_sprites.draw(screen)
-    draw_text("FPS: " + str(currentFPS), 22, WHITE, WIDTH/2, HEIGHT/10)
-    draw_text("Hitpoints: " + str(currentFPS), 22, WHITE, WIDTH/2, HEIGHT/20)
+    def show_start_screen(self):
+        pass
+    def show_go_screen(self):
+        pass
 
-    # buffer - after drawing everything, flip display
-    pg.display.flip()
+g = Game()
+while g.running:
+    g.new()
+
 
 pg.quit()
